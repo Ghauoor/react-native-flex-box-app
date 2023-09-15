bea2037ee68a82155a2da7c28b0d7f2f08bdaf4e
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-09-16 02:46:57 +0500

    responsive content

--- src/responsive_design/LayoutScreen.js
+++ src/responsive_design/LayoutScreen.js
@@ -13,7 +13,16 @@ const LayoutScreen = () => {
       {/* main view */}
       <View style={styles.main}>
         <View style={styles.section1} />
-        <View style={styles.section2} />
+        <View style={styles.section2}>
+          <Text style={styles.content}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
+            aliquid facere aliquam explicabo maxime excepturi at modi
+            voluptatibus obcaecati quisquam iure hic veniam eos vitae incidunt
+            qui, sequi illum? Expedita minima nisi illo repudiandae fuga,
+            nesciunt cupiditate velit quo fugiat consequatur assumenda? Quidem
+            voluptates quibusdam a aut molestiae unde tempora.
+          </Text>
+        </View>
       </View>
       {/* footer */}
       <View style={styles.footer} />
@@ -32,19 +41,22 @@ const styles = StyleSheet.create({
   main: {
     flex: 4,
     display: "flex",
-    flexDirection: "row",
+    // flexDirection: "row",
   },
   section1: {
     flex: 1,
     backgroundColor: "orange",
   },
   section2: {
-    flex: 1,
+    flex: 3,
     backgroundColor: "skyblue",
   },
   footer: {
     flex: 1,
     backgroundColor: "lightgreen",
+  },
+  content: {
+    fontSize: hp(3),
   },
 });
 
